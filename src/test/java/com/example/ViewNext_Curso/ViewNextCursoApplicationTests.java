d9ef4e714bb10fdf95c2ac76a708a609c0b917c4
Manuel Jesus <mjvazquez@viewnext.com>
2021-01-27 12:29:29 +0100

    Commit inicial

---
+++ src/test/java/com/example/ViewNext_Curso/ViewNextCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ViewNext_Curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewNextCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
